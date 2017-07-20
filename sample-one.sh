08d8e66c2d42ddc0f299f05ed00548f2d426bdfd
Dan Iter <dan_it_89@yahoo.com>
2017-07-20 01:35:50 +0000

    fixing for interfacing with client

--- sample-one.sh
+++ sample-one.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-THEANO_FLAGS=device=cuda0 /var/opt/wba/apps/anaconda2/bin/python sample_model.py -l tmp.txt  -f ../gold/f_sampling.pkl -s "$1" -b $2 -n 4
+THEANO_FLAGS=device=cuda0 /var/opt/wba/apps/anaconda2/bin/python sample_model.py -l "$1"  -f ../gold/f_sampling.pkl -b $2
